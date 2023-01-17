89b24287e76379e85dfe3c5df7207f61b715c4b9
xiaoyezi <f1ye@ucsd.edu>
2023-01-17 10:14:12 -0800

    version1

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+let items = ["buy food","cook food","eat food"];
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine","ejs")
 app.get("/",function(req,res){
-	res.send("hello");
+	let today = new Date();
+	let options = {
+		day : "numeric",
+		weekday : "long",
+		month : "long"
+	}
+	let day = today.toLocaleString("en-US",options);
+	res.render("index",{day :day, items: items});
 })
-
+app.post("/",function(req,res){
+	let item = req.body.newItem;
+	items.push(item);
+	res.redirect("/");
+})
 
 
 app.listen(3000,function(){
 	console.log("server running on port 3000");
 })
+
